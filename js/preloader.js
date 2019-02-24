8c5ee6ac19c1268e4f8ec41002b99837efced174
cyberhawksecurity <cyberhawksecurity@.github.com>
2019-02-25 00:46:38 +0530

    ff

--- js/preloader.js
+++ js/preloader.js
@@ -1,5 +1,13 @@
-var overlay = document.getElemenById("wrap");
+/* var overlay = document.getElemenById("wrap");
 
 window.addEventListener('load',function() {
 	overlay.styles.display = 'none'
+})
+
+body {
+  font-family: Purista,sans-serif, arial;
+  background: black;
+  color: #eaf7ff;
 }
+ */
+
