e0c5892766ec35a6dafa136e5a6c7bc81788cee9
cyberhawksecurity <cyberhawksecurity@.github.com>
2019-03-13 10:01:30 +0530

    chnge in insta

--- js/insta/custom.js
+++ js/insta/custom.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
         resolution: 'standard_resolution',
         accessToken: '7465858528.1677ed0.00ca62a77c204d76962e0a0a7f02687b',
         sortBy: 'most-recent',
-        template: '<div class="col-lg-3 instaimg"><a href="{{image}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',
+        template: '<div class="col-lg-2 instaimg"><a href="{{image}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',
     });
 
 
