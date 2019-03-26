f0651e8303da6c6d8bd79431b759dc3970fdab58
cyberhawksecurity <cyberhawksecurity@.github.com>
2019-03-26 07:17:31 +0530

    fiiii

--- seminars/index.js
+++ seminars/index.js
@@ -36,5 +36,45 @@ jQuery(function($) {
 
 });
 
+/************************
+ //* menu SETTINGS
+************************/
+
+
+function updateMenuButton() {
+	$('.js-menu-button').find('.menu_icon').toggleClass('is-active');
+}
+$switch = 0;
+function myFunction() {
+  document.getElementById("onn").style.display = "block";
+}
+function myFunction2() {
+  document.getElementById("onn").style.display = "none";
+}
+$(document).ready(function() {
+
+	$('.js-menu-button').click(function(e){
+
+		e.preventDefault();
+		updateMenuButton();
+		if( $switch == 0)
+		{
+			myFunction();
+			$switch =1;
+			
+		}
+		else
+		{
+			myFunction2();
+			$switch = 0;
+		}
+		
+
+	});
+
+});
+
+
+
 
 
